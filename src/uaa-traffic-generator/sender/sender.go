ece124699a3d14b2aa66511bf5b8539b30d0aff0
Dennis Leon <dleon@pivotal.io>
2018-10-13 15:45:44 -0700

    Add Function that will send 'GetMe' api traffic to the UAA

--- src/uaa-traffic-generator/sender/sender.go
+++ src/uaa-traffic-generator/sender/sender.go
@@ -1,5 +1,16 @@
 package sender
 
-func Send() {
+import "cloudfoundry-community/go-uaa/go-uaa"
 
+func Send(target string) {
+	api, err := uaa.NewWithPasswordCredentials(target, "", "", "", "", "", uaa.JSONWebToken)
+	if err != nil {
+		panic(err)
+	}
+
+	me, err := api.GetMe()
+	if err != nil {
+		panic(err)
+	}
+	println(me.Name)
 }
--- src/uaa-traffic-generator/sender/sender_test.go
+++ src/uaa-traffic-generator/sender/sender_test.go
@@ -5,6 +5,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
 	"uaa-traffic-generator/sender"
+	"cloudfoundry-community/go-uaa/go-uaa"
 )
 
 var _ = Describe("Sender", func() {
@@ -13,25 +14,32 @@ var _ = Describe("Sender", func() {
 
 	BeforeEach(func() {
 		fakeUaaServer = ghttp.NewServer()
+		fakeUaaServer.AllowUnhandledRequests = true
 	})
 
 	AfterEach(func() {
 		fakeUaaServer.Close()
 	})
 
-	Context("Send Obtain Token", func() {
+	Context("Send UserInfo Traffic", func() {
 		BeforeEach(func() {
-			fakeUaaServer.AppendHandlers(ghttp.CombineHandlers(
-				ghttp.VerifyRequest("POST", "/oauth/token", "a=a"),
-			))
+			fakeUaaServer.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("POST", "/oauth/token"),
+					ghttp.RespondWith(200, "{}"),
+				),
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("GET", "/userinfo", "scheme=openid"),
+					ghttp.RespondWithJSONEncoded(200, uaa.UserInfo{}),
+				),
+			)
 		})
 
 		It("should request a token from the UAA", func() {
 
-			sender.Send()
-			Expect(fakeUaaServer.ReceivedRequests()).To(Equal(1))
+			sender.Send(fakeUaaServer.URL())
+			Expect(fakeUaaServer.ReceivedRequests()).To(HaveLen(2))
 		})
 	})
-
 
 })
