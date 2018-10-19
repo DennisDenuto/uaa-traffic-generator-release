a01898bb2a8908e2ae3a729dab1cdc947057a735
Dennis Leon <dleon@pivotal.io>
2018-10-18 21:46:08 -0700

    Add concurrency when running traffic senders

--- src/uaa-traffic-generator/runner/runner.go
+++ src/uaa-traffic-generator/runner/runner.go
@@ -3,11 +3,19 @@ package runner
 import (
 	"uaa-traffic-generator/sender"
 	"github.com/cloudfoundry-community/go-uaa"
+	"sync"
 )
 
 func RunAll(api *uaa.API, senders []sender.TrafficSender) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(senders))
 
 	for _, s := range senders {
-		s.Send(api)
+		go func() {
+			defer wg.Done()
+			s.Send(api)
+		}()
 	}
+
+	wg.Wait()
 }
